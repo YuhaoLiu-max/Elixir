b8588be57b8486eaa81c4ca16e2ea5e787316c0f
YuhaoLiu-max <liuyuhao077@gmail.com>
2026-05-15 10:06:53 -0600

    Delete E1 directory

--- E1/run.exs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env elixir
-# run.exs – run the lexer without compiling a mix project:
-#   elixir run.exs sample1.py
-#   elixir run.exs sample1.py output.html
-
-Code.require_file("lib/python_lexer.ex", __DIR__)
-Code.require_file("lib/cli.ex", __DIR__)
-
-PythonLexer.CLI.main()
